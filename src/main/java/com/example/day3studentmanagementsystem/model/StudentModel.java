54c5537311ceaee76fcb4e1dc6e6e0dee383ab05
tushar90450 <ts3231440@gmail.com>
2026-01-20 12:48:31 +0530

    third

--- src/main/java/com/example/day3studentmanagementsystem/model/StudentModel.java
+++ src/main/java/com/example/day3studentmanagementsystem/model/StudentModel.java
@@ -3,12 +3,18 @@ package com.example.day3studentmanagementsystem.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Document(collection = "students")
 public class StudentModel {
+
+    @Id
     private String id;
     private String name;
     private int age;

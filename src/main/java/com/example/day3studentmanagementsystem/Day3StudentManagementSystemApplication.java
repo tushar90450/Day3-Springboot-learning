4f8e9a0c801fc0b992fb97a858d2e5287f99a072
tushar90450 <ts3231440@gmail.com>
2026-01-20 11:50:22 +0530

    first

---
+++ src/main/java/com/example/day3studentmanagementsystem/Day3StudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3StudentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3StudentManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day3studentmanagementsystem/Day3StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3StudentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

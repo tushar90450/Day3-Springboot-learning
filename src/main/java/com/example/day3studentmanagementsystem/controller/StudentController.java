c0b29f979099f683fdc40e7ed58638d7881f56c0
tushar90450 <ts3231440@gmail.com>
2026-02-09 12:12:15 +0530

    connect frontend

--- src/main/java/com/example/day3studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/example/day3studentmanagementsystem/controller/StudentController.java
@@ -9,7 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins= "*")
 @RestController
 public class StudentController {
 
